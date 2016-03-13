e7a9ebca4fd416c887f495486d262f40d4fe5b93
Theo van den Bogaart <todorus@todorus.com>
2016-03-13 19:20:03 +0100

    chained the promises in a recursive loop to insert all the data in the json array

--- importers/regionImporter.js
+++ importers/regionImporter.js
@@ -13,66 +13,83 @@ var Region = db["Region"];
 //  "pc2": 1000
 //}
 
-
-var i = 0;
-var data = json[i];
-var zip = null;
-
-Region.create(
-  {
-    name: String(data["PC"]),
-    type: Region.TYPE_ZIP
-  }
-).then(
-  function(result){
-    zip = result;
-
-    var data = null;
-    for(var j = 0; j < json.length; j++){
-      if(json[j]["PC"] == zip.name){
-        data = json[j];
-        break;
+function findOrCreate(properties){
+  return Region.findAndCountAll({where: properties}).then(
+    function(result){
+      if(result.count == 0){
+        return Region.create(properties);
+      } else {
+        return Region.findOne(properties);
       }
     }
+  )
+}
 
-    console.log("data",data,"zip",zip);
+function addData(data){
+  return findOrCreate(
+    {
+      name: String(data["PC"]),
+      type: Region.TYPE_ZIP
+    }
+  ).then(
+    function(result){
+      zip = result;
 
-    // PLACE
-    return Region.create(
-      {
-        name: String(data["PLAATS"]),
-        type: Region.TYPE_PLACE
-      }
-    )
-  }
-).then(
-  function(result){
-    var place = result;
-    zip.addParent(place);
+      // PLACE
+      return findOrCreate(
+        {
+          name: String(data["PLAATS"]),
+          type: Region.TYPE_PLACE
+        }
+      )
+    }
+  ).then(
+    function(result){
+      var place = result;
+      zip.addParent(place);
 
-    // MUNICIPALITY
-    return Region.create(
-      {
-        name: String(data["GEMEENTE"]),
-        type: Region.TYPE_MUNICIPALITY
-      }
-    );
-  }
-).then(
-  function(result){
-    var municipality = result;
-    zip.addParent(municipality);
+      // MUNICIPALITY
+      return findOrCreate(
+        {
+          name: String(data["GEMEENTE"]),
+          type: Region.TYPE_MUNICIPALITY
+        }
+      );
+    }
+  ).then(
+    function(result){
+      var municipality = result;
+      zip.addParent(municipality);
 
-    return Region.create(
-      {
-        name: String(data["PROVINCIE"]),
-        type: Region.TYPE_PROVINCE
-      }
-    )
+      return findOrCreate(
+        {
+          name: String(data["PROVINCIE"]),
+          type: Region.TYPE_PROVINCE
+        }
+      )
+    }
+  ).then(
+    function(result){
+      var province = result;
+      return zip.addParent(province);
+    }
+  )
+}
+
+
+
+function loopData(index, json){
+  if(index >= json.length){
+    return null;
   }
-).then(
-  function(result){
-    var province = result;
-    return zip.addParent(province);
-  }
-);
+
+  data = json[index];
+  return addData(data).then(
+    function(result){
+      index++
+      return loopData(index, json);
+    }
+  )
+}
+
+loopData(0, json);
