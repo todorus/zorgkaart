3dd084d6b33da7c9c29c9023edd1670d3920b113
Theo van den Bogaart <todorus@todorus.com>
2016-03-29 04:15:40 +0200

    client uses urlparams

--- client/app/services/region.service.ts
+++ client/app/services/region.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from 'angular2/core';
+import {Injectable} from "angular2/core";
 import {Region} from "../model/region";
-import {Http, Response} from "angular2/http";
+import {Http, Response, URLSearchParams} from "angular2/http";
 import {Observable} from "rxjs/Observable";
 
 
@@ -18,8 +18,11 @@ export class RegionService {
     private _regionsUrl = 'https://c0x7s177j4.execute-api.eu-west-1.amazonaws.com/development/regions';  // URL to web api
 
     fetch(query: string) {
-        // return this._http.get(this._regionsUrl, {query: query, limit: 10})
-        return this._http.get(this._regionsUrl)
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('query', query);
+        params.set('limit', '10');
+
+        return this._http.get(this._regionsUrl, {search: params})
             .map(res => <Region[]> res.json())
             .catch(this.handleError);
     }
