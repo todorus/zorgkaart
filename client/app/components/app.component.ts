fdc7bddd95b620f8c020551826258815ce8392b8
Theo van den Bogaart <todorus@todorus.com>
2016-03-30 02:02:54 +0200

    added leaflet map

--- client/app/components/app.component.ts
+++ client/app/components/app.component.ts
@@ -2,10 +2,12 @@ import {Component} from 'angular2/core';
 import {RegionService} from '../services/region.service';
 import {RegionSearchComponent} from './region-search.component';
 import {HTTP_PROVIDERS} from "angular2/http";
+import {MapComponent} from "./map.component";
 
 @Component({
   selector: 'my-app',
   template: `
+    <map></map>
     <div id="side_menu_container">
       <div id="side_menu">
           <h2>REGIO</h2>
@@ -13,7 +15,7 @@ import {HTTP_PROVIDERS} from "angular2/http";
       </div>
     </div>
   `,
-  directives: [RegionSearchComponent],
+  directives: [RegionSearchComponent, MapComponent],
   providers: [RegionService, HTTP_PROVIDERS]
 })
 export class AppComponent { }
