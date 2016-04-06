44a2f8938f74949943cbce804380c36f6b137415
Theo van den Bogaart <todorus@todorus.com>
2016-04-06 23:18:47 +0200

    added merge service

---
+++ api/nodejscomponent/regions/merge/handler.js
@@ -0,0 +1,84 @@
+'use strict';
+
+/**
+ * Serverless Module: Lambda Handler
+ * - Your lambda functions should be a thin wrapper around your own separate
+ * modules, to keep your code testable, reusable and AWS independent
+ * - 'serverless-helpers-js' module is required for Serverless ENV var support.  Hopefully, AWS will add ENV support to Lambda soon :)
+ */
+
+// Require Serverless ENV vars
+var env = require('serverless-helpers-js').loadEnv();
+
+// Require Logic
+var turf = require('turf');
+var db = require(__dirname + '/../../lib/models');
+var Region = db["Region"];
+
+// Lambda Handler
+module.exports.handler = function (event, context) {
+
+  var type = Region.TYPE_ZIP;
+  // Serverless does not support parsed query parameters, but rather a string. This is a workaround until this issue
+  // is resolved
+  var idArray = JSON.parse(event["regions"])
+  var ids = idArray.join();
+
+  db.sequelize.query(
+    "SELECT * FROM \"Regions\" WHERE id IN (" + ids + ") ",
+    { type: db.sequelize.QueryTypes.SELECT}
+  ).then(
+    function (result) {
+
+      var zips = [];
+      for(var i = 0; i < result.length; i++){
+        var region = result[i];
+        for(var j=0; region.zips != null && j < region.zips.length; j++){
+          var zip = region.zips[j];
+          if(zips.indexOf(zip) == -1){
+            zips.push(zip);
+          }
+        }
+      }
+
+      var zipsString = "'"+zips.join("','")+"'";
+
+      return db.sequelize.query(
+        "SELECT * FROM \"Regions\" WHERE name IN (" + zipsString + ") AND area IS NOT NULL",
+        { type: db.sequelize.QueryTypes.SELECT}
+      )
+
+    },
+    function (error) {
+      context.fail(error);
+    }
+  ).then(
+    function (result) {
+
+      var polygons = {
+        type: "FeatureCollection",
+        features: []
+      };
+
+      for(var i = 0; i < result.length; i++){
+        var region = result[i];
+        region.area["properties"] = {};
+        polygons["features"].push(region.area);
+      }
+
+      var merged = turf.merge(polygons);
+
+      var response = {
+        id: null,
+        name: null,
+        type: null,
+        area: merged
+      }
+      context.succeed(response);
+    },
+    function (error) {
+      context.fail(error);
+    }
+  )
+
+};
---
+++ api/test/regions/merge_spec.js
@@ -0,0 +1,186 @@
+require('../spec_helper.js');
+
+var subject = require("../../nodejscomponent/regions/merge/handler.js");
+var db = require("../../nodejscomponent/lib/models");
+var Region = db["Region"];
+
+describe("Region", function () {
+
+  // testdata from http://turfjs.org/static/docs/module-turf_merge.html
+
+  var polyA = {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          9.994812,
+          53.549487
+        ],
+        [
+          10.046997,
+          53.598209
+        ],
+        [
+          10.117721,
+          53.531737
+        ],
+        [
+          9.994812,
+          53.549487
+        ]
+      ]
+    ]
+  };
+
+  var polyB = {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          10.000991,
+          53.50418
+        ],
+        [
+          10.03807,
+          53.562539
+        ],
+        [
+          9.926834,
+          53.551731
+        ],
+        [
+          10.000991,
+          53.50418
+        ]
+      ]
+    ]
+  };
+
+  var polyAB = {
+    "type": "Polygon",
+    "coordinates": [
+      [
+        [
+          10.005390809136088,
+          53.55936379867258
+        ],
+        [
+          10.046997,
+          53.598209
+        ],
+        [
+          10.117721,
+          53.531737
+        ],
+        [
+          10.026838636912657,
+          53.54486184801601
+        ],
+        [
+          10.000991,
+          53.50418
+        ],
+        [
+          9.926834,
+          53.551731
+        ],
+        [
+          10.005390809136088,
+          53.55936379867258
+        ]
+      ]
+    ]
+  }
+
+  before(
+    function (done) {
+      // Wipe db
+      db.sequelize.sync(
+        {force: true}
+      ).then(
+        function (result) {
+
+          // Define zipcodes
+          return Region.bulkCreate(
+            [
+              {id: 1, name: "Maas", type: Region.TYPE_PLACE, zips: [1001, 1002, 1003]},
+              {
+                id: 10,
+                name: '1001',
+                type: Region.TYPE_ZIP,
+                area: {type: "Feature", properties: {name: '1001'}, geometry: polyA},
+                zips: [1001]
+              },
+              {
+                id: 11,
+                name: '1002',
+                type: Region.TYPE_ZIP,
+                area: {type: "Feature", properties: {name: '1002'}, geometry: polyB},
+                zips: [1002]
+              }
+            ]
+          )
+        }
+      ).then(
+        function (result) {
+          done();
+        },
+        function (error) {
+          throw error;
+        }
+      )
+    }
+  )
+
+  describe("merge", function () {
+
+    describe("with Region that has multiple zipcodes", function () {
+
+      var event = {
+        regions: "[ 1 ]"
+      }
+
+      var matchingRegion = {id: null, name: null, type: null, area: {type: "Feature", properties: {}, geometry: polyAB} };
+
+      it("should return a list of all zipcodes ordered by name", function (done) {
+        var context = new MockContext()
+        context.then(
+          function (context) {
+
+            expect(context.error).toBe(null);
+            expect(context.response).toEqual(matchingRegion);
+
+            done();
+          }
+        );
+
+        subject.handler(event, context);
+      });
+    })
+
+    describe("with zipcodes", function () {
+
+      var event = {
+        regions: "[ 10, 11]"
+      }
+
+      var matchingRegion = {id: null, name: null, type: null, area: {type: "Feature", properties: {}, geometry: polyAB} };
+
+      it("should return a list of all zipcodes ordered by name", function (done) {
+        var context = new MockContext()
+        context.then(
+          function (context) {
+
+            expect(context.error).toBe(null);
+            expect(context.response).toEqual(matchingRegion);
+
+            done();
+          }
+        );
+
+        subject.handler(event, context);
+      });
+    })
+  })
+
+});
