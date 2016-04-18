5862ab7d148c7dfb544b3ab6e602bf3f33e82179
Theo van den Bogaart <todorus@todorus.com>
2016-04-18 16:40:41 +0200

    refactored connectioncreator to only have one connection open at a time, as the http server of neo4j can get overloaded

--- api/importers/connection_creator.js
+++ api/importers/connection_creator.js
@@ -1,45 +1,53 @@
 var fs = require('fs'),
-  JSONStream = require('JSONStream'),
-  es = require('event-stream'),
   db = require(__dirname + '/../nodejscomponent/lib/models');
 
 var Region = db["Region"];
 var CONTAINS = db["CONTAINS"];
 
-var connectionData = 'postcode-gemeente-tabel.json',
-  connectStream = fs.createReadStream(connectionData, {encoding: 'utf8'}),
-  parser = JSONStream.parse('*');
+var connectionData = 'postcode-gemeente-tabel.json'
+var json = JSON.parse(fs.readFileSync(connectionData, 'utf8'));
+var index = -1;
 
-connectStream
-  .pipe(parser)
-  .pipe(es.mapSync(function (data) {
-    {
-      var _data = data;
-      var municipalityCode = _data["Gemeentecode"];
-      var zipCode = _data['Postcode'];
+increment();
 
-      var props = {
-        parent: {
-          type: Region.TYPE_MUNICIPALITY,
-          code: municipalityCode
-        },
-        child: {
-          type: Region.TYPE_ZIP,
-          code: zipCode
-        }
-      };
 
-      var contains = CONTAINS.build(props);
-      contains.save()
-        .then(
-          function (result) {
-            console.log("result", result.length > 0);
-          }
-        ).catch(
-          function (error) {
-            console.error(error);
-          }
-        );
+function increment(){
+  index++;
+  if(index >= json.length){
+    return;
+  }
 
+  process(index)
+    .then(
+      function (result) {
+        console.log("result", result.length > 0);
+        increment();
+      }
+    ).catch(
+    function (error) {
+      console.error(error);
+      increment();
     }
-  }));
+  )
+}
+
+function process(i) {
+  var data = json[i];
+  var municipalityCode = data["Gemeentecode"];
+  var zipCode = data['Postcode'];
+
+  var props = {
+    parent: {
+      type: Region.TYPE_MUNICIPALITY,
+      code: municipalityCode
+    },
+    child: {
+      type: Region.TYPE_ZIP,
+      code: zipCode
+    }
+  };
+
+  var contains = CONTAINS.build(props);
+  return contains.save();
+
+}
