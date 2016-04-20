92845509cefcfa356404c747b3e25ff67332e999
Theo van den Bogaart <todorus@todorus.com>
2016-04-20 17:11:23 +0200

    refactored zip importer to use a recursive promises pattern to ensure that the target db does not get overloaded with requests

--- api/importers/zip_geojson_creator.js
+++ api/importers/zip_geojson_creator.js
@@ -1,51 +1,69 @@
 var fs = require('fs'),
-  JSONStream = require('JSONStream'),
-  es = require('event-stream'),
   db = require(__dirname + '/../nodejscomponent/lib/models');
 
 var Region = db["Region"];
+var CONTAINS = db["CONTAINS"];
 
-var jsonData = 'postcodes.geo.json',
-  stream = fs.createReadStream(jsonData, {encoding: 'utf8'}),
-  parser = JSONStream.parse('*');
+var jsonData = 'postcodes.geo.json';
+var json = JSON.parse(fs.readFileSync(jsonData, 'utf8'));
+var index = -1;
 
-stream
-  .pipe(parser)
-  .pipe(es.mapSync(function (data) {
-    {
-      var _data = data;
-      var name = _data["properties"]["PC4"].toString();
-      var code = name;
+increment();
 
-      Region.findOrCreate({
+
+function increment(){
+  index++;
+  if(index >= json.length){
+    return;
+  }
+
+  process(index)
+    .then(
+      function (result) {
+        increment();
+      }
+    ).catch(
+    function (error) {
+      console.error(error);
+      increment();
+    }
+  )
+}
+
+function process(i) {
+  var data = json[i];
+  var name = data["properties"]["PC4"].toString();
+  var code = name;
+
+  return Region.findOrCreate({
+    name: name,
+    type: Region.TYPE_ZIP,
+    code: code
+  }).then(
+    function (result) {
+      var region = result[0];
+      region.set("description", null);
+
+      var area = data;
+      area["properties"] = {
+        id: region.properties["_id"],
         name: name,
-        type: Region.TYPE_ZIP,
-        code: code
-      }).then(
-        function (result) {
-          var region = result[0];
-          region.set("description", null);
+        description: null,
+        type: Region.TYPE_ZIP
+      };
+      region.set("area", JSON.stringify(area));
 
-          var area = _data;
-          area["properties"] = {
-            id: region.properties["_id"],
-            name: name,
-            description: null,
-            type: Region.TYPE_ZIP
-          };
-          region.set("area", JSON.stringify(area));
+      return region.save();
+    }
+  ).then(
+    function (result) {
+      console.log("result", result);
+    }
+  ).catch(
+    function (error) {
+      console.error(error);
+    }
+  );
 
-          return region.save();
-        }
-      ).then(
-        function (result) {
-          console.log("result", result);
-        }
-      ).catch(
-        function (error) {
-          console.error(error);
-        }
-      );
+}
 
-    };
-  }));
