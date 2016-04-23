3737c1935c119d59fd161b7e342feffe4f15b34c
Theo van den Bogaart <todorus@todorus.com>
2016-04-23 15:12:30 +0200

    implemented a simple create handler for regions

--- api/nodejscomponent/regions/create/handler.js
+++ api/nodejscomponent/regions/create/handler.js
@@ -1,7 +1,66 @@
 'use strict';
 
-module.exports.handler = function(event, context) {
-  return context.done(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
+/**
+ * Serverless Module: Lambda Handler
+ * - Your lambda functions should be a thin wrapper around your own separate
+ * modules, to keep your code testable, reusable and AWS independent
+ * - 'serverless-helpers-js' module is required for Serverless ENV var support.  Hopefully, AWS will add ENV support to Lambda soon :)
+ */
+
+// Require Serverless ENV vars
+var env = require('serverless-helpers-js').loadEnv();
+
+// Require Logic
+var turf = require('turf');
+var db = require(__dirname + '/../../lib/models');
+var Region = db["Region"];
+
+// Lambda Handler
+module.exports.handler = function (event, context) {
+
+  var ids = event["children"].join();
+  var regions = [];
+  var properties = {
+    name: event["name"],
+    description: event["description"],
+    type: Region.TYPE_CARE
+  };
+
+  Region.byIds(ids)
+    .then(function (result) {
+      regions = regions.concat(result);
+      return Region.children(ids);
+    }).then(function (result) {
+    regions = regions.concat(result);
+
+    var polygons = {
+      type: "FeatureCollection",
+      features: []
+    };
+
+    for (var i = 0; i < regions.length; i++) {
+      var region = regions[i];
+      var area = region.data["area"];
+
+      if (area != undefined && area != null) {
+        area["properties"] = {};
+        polygons["features"].push(area);
+      }
+    }
+
+    var merged = turf.merge(polygons);
+    properties["area"] = JSON.stringify(merged);
+
+    return Region.create(properties);
+
+  }).then(function(result){
+
+    var response = Region.toJson(result[0].data);
+    context.succeed(response);
+
+  }).catch(function (error) {
+    context.fail(error);
   });
+
+
 };
