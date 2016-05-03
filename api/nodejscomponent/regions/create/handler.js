7686cb76e0bce32e3632e13b41dc8dea13a4ca9f
Theo van den Bogaart <todorus@todorus.com>
2016-05-03 18:53:47 +0200

    added a TODO to use transactions to make creating a Region an atomic function

--- api/nodejscomponent/regions/create/handler.js
+++ api/nodejscomponent/regions/create/handler.js
@@ -1,11 +1,6 @@
 'use strict';
 
-/**
- * Serverless Module: Lambda Handler
- * - Your lambda functions should be a thin wrapper around your own separate
- * modules, to keep your code testable, reusable and AWS independent
- * - 'serverless-helpers-js' module is required for Serverless ENV var support.  Hopefully, AWS will add ENV support to Lambda soon :)
- */
+//TODO use a transaction to make to create the new Region and connect it to its children in one action
 
 // Require Serverless ENV vars
 var env = require('serverless-helpers-js').loadEnv();
