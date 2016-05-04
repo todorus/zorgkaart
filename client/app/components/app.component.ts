5ddaf812499839390ac9385182d69f79449902e9
Theo van den Bogaart <todorus@todorus.com>
2016-05-04 15:53:21 +0200

    got basic routing working

--- client/app/components/app.component.ts
+++ client/app/components/app.component.ts
@@ -3,18 +3,19 @@ import {RegionService} from "../services/region.service";
 import {HTTP_PROVIDERS} from "angular2/http";
 import {MapComponent} from "./map.component";
 import {RegionFormComponent} from "./region-form.component";
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from "angular2/router";
 
 @Component({
   selector: 'my-app',
   template: `
     <map></map>
-    <div id="side_menu_container">
-      <div id="side_menu">
-          <region-form></region-form>
-      </div>
-    </div>
+    <router-outlet></router-outlet>
   `,
-  directives: [RegionFormComponent, MapComponent],
-  providers: [RegionService, HTTP_PROVIDERS]
+  directives: [ROUTER_DIRECTIVES, RegionFormComponent, MapComponent],
+  providers: [RegionService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
+@RouteConfig([
+  {path: '/region/create',        component: RegionFormComponent, name:"RegionCreate"},
+  {path: '/**',      redirectTo: ['RegionCreate']}
+])
 export class AppComponent { }
