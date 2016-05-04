34fdca21b0fd38d735baa8acfe53a7778e3bd18d
Theo van den Bogaart <todorus@todorus.com>
2016-05-04 21:27:45 +0200

    created a placeholder welcome screen and some basic routing to test switching between screens

--- client/app/components/app.component.ts
+++ client/app/components/app.component.ts
@@ -4,18 +4,25 @@ import {HTTP_PROVIDERS} from "angular2/http";
 import {MapComponent} from "./map.component";
 import {RegionFormComponent} from "./region-form.component";
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from "angular2/router";
+import {StartComponent} from "./start.component";
 
 @Component({
   selector: 'my-app',
   template: `
     <map></map>
     <router-outlet></router-outlet>
+    <nav>
+      <a [routerLink]="['/Region']">
+          <img src="app/img/login_ic.svg" />
+      </a>
+    </nav>
+    
   `,
   directives: [ROUTER_DIRECTIVES, RegionFormComponent, MapComponent],
   providers: [RegionService, HTTP_PROVIDERS, ROUTER_PROVIDERS]
 })
 @RouteConfig([
-  {path: '/region/create',        component: RegionFormComponent, name:"RegionCreate"},
-  {path: '/**',      redirectTo: ['RegionCreate']}
+  {path: '/region', component: RegionFormComponent, as:"Region"},
+  {path: '/',       component: StartComponent,      as:"Start"}
 ])
 export class AppComponent { }
